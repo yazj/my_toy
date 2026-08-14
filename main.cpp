7ef21609046794106d4b6f24ce9d200ae4f6f76b
yazj <yuanyi339@gmail.com>
2026-08-14 17:17:23 +0800

    添加raygui库

--- main.cpp
+++ main.cpp
@@ -1,19 +1,35 @@
 #include "raylib.h"
 
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
 int main() {
-    constexpr int screenWidth = 800;
-    constexpr int screenHeight = 450;
+  InitWindow(1280, 960, "raygui - controls test suite");
+  SetTargetFPS(60);
 
-    InitWindow(screenWidth, screenHeight, "raylib [CMake + FetchContent] - Standalone Test");
-    SetTargetFPS(60);
+  bool showMessageBox = false;
 
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first raylib window!", 190, 200, 20, LIGHTGRAY);
-        EndDrawing();
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+
+    if (GuiButton(Rectangle{.x = 24, .y = 24, .width = 120, .height = 30},
+                  "#191#Show Message"))
+      showMessageBox = true;
+
+    if (showMessageBox)
+
+    {
+      GuiMessageBox(Rectangle{.x = 85, .y = 70, .width = 500, .height = 200},
+                    "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+
+      if constexpr (constexpr int btnActive = -1; btnActive >= 0)
+        showMessageBox = false;
     }
 
-    CloseWindow();
-    return 0;
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
 }
