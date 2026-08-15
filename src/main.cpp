9f1946eded1e9bdde66a9eeb958cdf4d5ad6d785
yazj <yuanyi339@gmail.com>
2026-08-16 03:15:36 +0800

    添加 rlImGui

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,26 @@
+#include "imgui.h"
 #include "raylib.h"
-
-#define RAYGUI_IMPLEMENTATION
-#include "raygui.h"
+#include "rlImGui.h"
 
 int main() {
-  InitWindow(720, 960, "");
+  InitWindow(1280, 960, "my_toy");
   SetTargetFPS(60);
+
+  // 初始化 rlImGui（绑定 raylib 的渲染后端）
+  rlImGuiSetup(true);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
-
     ClearBackground(BLACK);
+
+    rlImGuiBegin();
+    ImGui::ShowDemoWindow();
+    rlImGuiEnd();
 
     EndDrawing();
   }
 
+  rlImGuiShutdown();
   CloseWindow();
   return 0;
 }
